62558f7b5f0d6aac12089f6c2ac901ff3697bf54
Jay <jupadhyay1@yahoo.com>
2019-06-11 08:10:48 -0700

    Still not working but added some stuff

--- Source.cpp
+++ Source.cpp
@@ -1,14 +1,23 @@
 #include <iostream>
 #include "functions.h"
 using namespace std;
+bool errcheck(int,char);
+
 
 int main()
 {
+	bool error=false;
 	double num1, num2;
 	char operation;
-	cout << "Enter math statement." << endl;
-	cin >> num1;
-	cin >> operation;
-	cin >> num2;
+	do // Loops for input until error // NEED TO CHECK!
+	{
+		error=false;
+		cout << "Enter math statement." << endl;
+		while(cin>>num1||!cin) // continues to get number and operations until end of stream
+		{
+			cin >> operation;
+			error=errcheck(num1,operation);
+		}
+	}while(error);
 	calc(num1, num2, operation);
 }
--- functions.h
+++ functions.h
@@ -19,3 +19,22 @@ void calc(float num1, float num2, char operation)
 		cout << num1 / num2 << endl;
 	}
 }
+
+
+bool errcheck(int num1, char op)
+{
+	//May need to add an additional checker for bad data. Not sure though because everything is either integer or character?
+	if (num1>1000)
+	{
+		cout << "Number is too high or low. Please try again."<< endl;
+		return true;
+
+	}
+	if(op!='+'&&op!='-'&&op!='*'&&op!='/'&&op!='^')
+	{
+		cout << "Please try again. The operation needs to be valid." << endl;
+		return true;
+	}
+	return false;
+
+}
