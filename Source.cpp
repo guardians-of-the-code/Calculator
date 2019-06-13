2c4c0bace827e752f1a777db4496b9299f2bdb9f
Daniel Stahl <stahl@Daniels-MacBook-Pro.local>
2019-06-12 21:04:55 -0700

    program looping after user input

--- Source.cpp
+++ Source.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <stdlib.h>
 #include "functions.h"
 
 using namespace std;
@@ -10,6 +11,15 @@ int main()
 {
 	vector<double> nums;// Declare vector
 	vector <char> op;
-	getin(nums,op);
-	calc(nums.at(0), nums.at(1), op.at(0));
+	char userInput;
+
+	while (userInput != 'q' && userInput != 'Q')
+	{
+		getin(nums,op);
+		calc(nums.at(0), nums.at(1), op.at(0));
+
+		cout << "Press the Enter key to continue, press Q or q to quit..." << endl;
+		cin.get(userInput);
+	}
+	
 }
