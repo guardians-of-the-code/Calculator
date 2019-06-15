98bacb91018300311e0a132f199414880461708e
Jay <jupadhyay1@yahoo.com>
2019-06-15 15:48:15 -0700

    Got rid of white space. Checked to see if working

--- Source.cpp
+++ Source.cpp
@@ -2,29 +2,22 @@
 #include <vector>
 #include <stdlib.h>
 #include "functions.h"
-
 using namespace std;
-
-vector<double> nums;// Declare vector
-vector <char> op;
-
 int main()
-
 {
 	CalcClass calc;
+	vector<double> nums;// Declare vector
+	vector <char> op;
 	char userInput = 'a';
 
 	while (userInput != 'q' && userInput != 'Q')
 	{
-
 		calc.getin(nums, op);
 		calc.calc(nums.at(0), nums.at(1), op.at(0));
-
-		cout << "Press the Enter key to continue, press Q or q to quit..." << endl;
-		cin.get(userInput);
 		nums.clear();
 		op.clear();
+		cout << "Press the Enter key to continue, press Q or q to quit..." << endl;
+		cin.get(userInput);
+
 	}
-
-
 }
--- functions.h
+++ functions.h
@@ -8,8 +8,6 @@ class CalcClass
 {
 public:
 
-
-
 	//TODO FOR OBJECT ORIENTED
 
 
