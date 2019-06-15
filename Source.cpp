2689614d0dea1d64577299f9f4ec6c36838191ec
Jay U <36392692+twotonerebel3@users.noreply.github.com>
2019-06-15 12:11:04 -0700

    works
    works

--- Source.cpp
+++ Source.cpp
@@ -1,15 +1,28 @@
 #include <iostream>
 #include <vector>
+#include <stdlib.h>
 #include "functions.h"
 
 using namespace std;
 
-
+vector<double> nums;// Declare vector
+vector <char> op;
 
 int main()
-{
-	vector<double> nums;// Declare vector
-	vector <char> op;
-	getin(nums,op);
-	calc(nums.at(0), nums.at(1), op.at(0));
+{	
+	CalcClass calc;
+	char userInput='a';
+
+	while (userInput != 'q' && userInput != 'Q')
+	{
+		
+		calc.getin(nums, op);
+		calc.calc(nums.at(0), nums.at(1), op.at(0));
+
+		cout << "Press the Enter key to continue, press Q or q to quit..." << endl;
+		cin.get(userInput);
+		nums.clear();
+		op.clear();
+	}
+
 }
