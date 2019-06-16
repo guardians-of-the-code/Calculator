d7e7ba87ee48b605e4948f8c9cdc250d98b1aec7
Jay <jupadhyay1@yahoo.com>
2019-06-16 13:32:54 -0700

    Longer operation support

--- Source.cpp
+++ Source.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 vector<double> nums;// Declare vector
 vector <char> op;
-
+double solved;
 int main()
 
 {
@@ -18,8 +18,15 @@ int main()
 	{
 
 		calc.getin(nums, op);
-		calc.calc(nums.at(0), nums.at(1), op.at(0));
-
+		while (nums.size() >= 2)
+		{														
+			solved=calc.calc(nums.at(0), nums.at(1), op.at(0));// 
+			nums.erase(nums.begin());// Deletes first number
+			nums.at(0)=solved; //places first number with the solution of both numbers
+			op.erase(op.begin());// Deletes first operation character meaning next operation is now first operation
+			
+		}
+		cout << solved << " is the solution." << endl;
 		cout << "Press the Enter key to continue, press Q or q to quit..." << endl;
 		cin.get(userInput);
 		nums.clear();
